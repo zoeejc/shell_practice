c03b7ce68a22388625e19d59726f27337409d290
Zoe Collins <zoe.collins2@mail.dcu.ie>
2023-03-14 15:30:04 +0000

    Getting tokens from stdin

--- myshell.c
+++ myshell.c
@@ -7,11 +7,24 @@
 
 int main(int argc, char * argv[]) {
 
-    char symbol = ">>>";
+    char * symbol = ">>>";
+    char buffer[MAX_BUFFER]; // setting max length
+    char * args[MAX_ARGS]; // setting max length
 
     while (!feof(stdin)) { // main loop until end of file/interrupt
-        fputs(symbol, stdout); // print the symbol
-        if (fgets())
+        fputs(symbol, stdout); // print the symbol (writing to standard output)
+        if (fgets(buffer, MAX_BUFFER, stdin)) {
+            int i = 0;
+            char ** hold = strtok(buffer, SEPARATORS); // 'hold' holds the result of tokenising so we can add to args
+            while (hold != NULL && i < MAX_ARGS) { // while there is something to tokenise, and we aren't over max length
+                args[i] = hold; // add token to list
+                i++;
+                hold = strtok(buffer, SEPARATORS); // get next token ready
+            }
+
+            args[i] = NULL; // last element must be null
+
+        }
 
     }
 
