aec56b9b2d4003b3bf87a989e96607e83f479647
Zoe Collins <zoe.collins2@mail.dcu.ie>
2023-03-14 14:28:17 +0000

    working on main

--- myshell.c
+++ myshell.c
@@ -3,4 +3,18 @@
 #include <stdlib.h>
 #define MAX_BUFFER 1024 // line buffer
 #define MAX_ARGS 64
-#define SEPARATORS " \t\n"  // token separators (space, tab, newline)    
+#define SEPARATORS " \t\n"  // token separators (space, tab, newline)
+
+int main(int argc, char * argv[]) {
+
+    char symbol = ">>>";
+
+    while (!feof(stdin)) { // main loop until end of file/interrupt
+        fputs(symbol, stdout); // print the symbol
+        if (fgets())
+
+    }
+
+    return 0;
+
+}
